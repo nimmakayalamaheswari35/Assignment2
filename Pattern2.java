006e13a267adc8b37512236ffde9ed6a5ff718ed
nimmakayalamaheswari35 <84019315+nimmakayalamaheswari35@users.noreply.github.com>
2021-05-17 18:05:31 +0530

    Add files via upload

---
+++ Pattern2.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+class Pattern2
+{
+	public static void main(String[] args)
+	{
+		Scanner t=new Scanner(System.in);
+		System.out.println("Enter the name");	
+		String name=t.next();
+		int r=8;
+		int c=22;
+		for(int i=0;i<r;i++){
+
+			for(int j=0;j<c;j++)
+			{
+				if((i==0&&j==0)||(i==0&&j==c-1)||(i==r-1&&j==0)||(i==r-1&&j==c-1))
+				System.out.print("+");
+				else if(i==0||i==r-1)
+				System.out.print("-");
+				else if(j==0||j==c-1)
+				System.out.print("|");
+				else if(i==r-3&&j==c-(name.length()+2))
+				{
+					for(int k=0;k<name.length();k++)
+					System.out.print(name.charAt(k));
+					j=j+name.length()-1;
+				}
+				else
+				System.out.print(" ");
+				}
+				System.out.println();
+			}
+		}
+	}
